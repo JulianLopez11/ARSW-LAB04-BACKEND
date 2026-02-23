15752b16f1e46a8cf700b8a13ee4f700b3bc4159
Julian Lopez <julian.lopez-b@mail.escuelaing.edu.co>
2026-02-22 19:28:34 -0500

    fix

--- src/main/java/edu/eci/arsw/blueprints/controllers/BluePrintWebSocketController.java
+++ src/main/java/edu/eci/arsw/blueprints/controllers/BluePrintWebSocketController.java
@@ -1,36 +1,32 @@
 package edu.eci.arsw.blueprints.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import edu.eci.arsw.blueprints.dto.BluePrintUpdate;
 import edu.eci.arsw.blueprints.dto.DrawEvent;
-import edu.eci.arsw.blueprints.model.Point;
-
+import edu.eci.arsw.blueprints.model.Blueprint;
+import edu.eci.arsw.blueprints.services.BlueprintsServices;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import java.util.List;
-//PARA STOMP
+import org.springframework.stereotype.Controller;
+
 @Controller
 public class BluePrintWebSocketController {
 
-  private final SimpMessagingTemplate template;
+    private final SimpMessagingTemplate template;
+    private final BlueprintsServices services; // Usamos el mismo servicio que el API REST
 
-  public BluePrintWebSocketController(SimpMessagingTemplate template) {
-    this.template = template;
-  }
+    public BluePrintWebSocketController(SimpMessagingTemplate template, BlueprintsServices services) {
+        this.template = template;
+        this.services = services;
+    }
 
-  @MessageMapping("/draw")
-  public void onDraw(DrawEvent evt) {
-    var upd = new BluePrintUpdate(evt.author(), evt.name(), List.of(evt.point()));
-    template.convertAndSend("/topic/blueprints." + evt.author() + "." + evt.name(), upd);
-  }
-
-  @ResponseBody
-  @GetMapping("/api/v1/blueprints/{author}/{name}")
-  public BluePrintUpdate get(@PathVariable String author, @PathVariable String name) {
-    return new BluePrintUpdate(author, name, List.of(new Point(10,10), new Point(40,50)));
-  }
+    @MessageMapping("/draw")
+    public void onDraw(DrawEvent evt) {
+        try {
+            services.addPoint(evt.author(), evt.name(), evt.point().x(), evt.point().y());
+            Blueprint updatedBp = services.getBlueprint(evt.author(), evt.name());
+            template.convertAndSend("/topic/blueprints." + evt.author() + "." + evt.name(), updatedBp);
+            
+        } catch (Exception e) {
+            System.err.println("Error en dibujo RT: " + e.getMessage());
+        }
+    }
 }
